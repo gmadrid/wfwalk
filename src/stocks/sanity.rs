18d99bc3f6a9c44e9a07ae8872d742de6ab0dda0
George Madrid <gmadrid@gmail.com>
2019-02-28 00:48:22 -0500

    Adding NTree::find_node and using it were apropos. (Some code cleanup and reformatting.

--- src/stocks/sanity.rs
+++ src/stocks/sanity.rs
@@ -1,11 +1,54 @@
+use super::Stock;
+
 type Insanity = String;
 
-trait SanityCheck {
-    fn sanity_check(&self) -> Vec<Insanity>;
+fn sanity_check(stock: Stock) -> Vec<Insanity> {
+    let mut insanities = vec![];
+
+    let tests = vec![
+        has_name,
+        has_brokerage_tag,
+        has_short_tag_if_needed,
+        has_portfolio_tag,
+        has_lots,
+    ];
+
+    for test in tests {
+        test(stock).map(|i| insanities.push(i));
+    }
+
+    insanities
 }
 
-impl SanityCheck for super::Stock {
-    fn sanity_check(&self) -> Vec<Insanity> {
-        unimplemented!()
-    }
+//trait MapNone {
+//    fn map_none<U, F>(&self, f: F) -> Option<U> where F: FnOnce() -> U;
+//}
+//
+//impl<T> MapNone for Option<T> {
+//    fn map_none<U, F>(&self, f: F) -> Option<U> where F: FnOnce() -> U {
+//        match self {
+//            Some(_) => None,
+//            None => Some(f())
+//        }
+//    }
+//}
+//
+fn has_name(stock: Stock) -> Option<Insanity> {
+    unimplemented!()
+}
+
+fn has_brokerage_tag(stock: Stock) -> Option<Insanity> {
+    unimplemented!()
+}
+
+fn has_short_tag_if_needed(stock: Stock) -> Option<Insanity> {
+    unimplemented!()
+}
+
+fn has_portfolio_tag(stock: Stock) -> Option<Insanity> {
+    unimplemented!()
+}
+
+fn has_lots(stock: Stock) -> Option<Insanity> {
+    unimplemented!()
 }
