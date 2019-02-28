5f988a4bd27a4332689a2c64895032c80925114a
George Madrid <gmadrid@gmail.com>
2019-02-28 10:09:34 -0500

    Moving errors mod to own file.

--- src/lib.rs
+++ src/lib.rs
@@ -6,24 +6,9 @@ extern crate error_chain;
 #[macro_use]
 extern crate lazy_static;
 
+pub mod errors;
 pub mod stocks;
 pub mod tree;
 mod type_tools;
-
-pub mod errors {
-    error_chain! {
-        errors {
-            BadParse(nonterminal: String, desc: String, _text: String) {
-                description("a parse error"),
-                display("Parse error: {:1} {:0}", desc, nonterminal),
-            }
-        }
-
-        foreign_links{
-            Io(::std::io::Error);
-            ParseFloat(::std::num::ParseFloatError);
-        }
-    }
-}
 
 use type_tools::{BoolTools, OptionTools, VecTools};
