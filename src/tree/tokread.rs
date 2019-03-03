fc7a13c7b5a5d0e55aef6113a880c97b397e6a5b
George Madrid <gmadrid@gmail.com>
2019-03-03 11:13:56 -0500

    Fixing type annotations in tokread.rs.

--- src/tree/tokread.rs
+++ src/tree/tokread.rs
@@ -1,24 +1,26 @@
+use crate::errors::*;
 use crate::tree::build_data::BuildData;
-use futures::future::FutureResult;
 use std::io::BufReader;
 use std::path::PathBuf;
 use tokio::fs::File;
 use tokio::io::lines;
 use tokio::prelude::*;
 
-pub fn read_tree_async<P>(path: P) -> impl Future<Item = BuildData, Error = std::io::Error>
+pub fn read_tree_async<P>(path: P) -> impl Future<Item = BuildData, Error = Error>
 where
     P: Into<PathBuf>,
 {
-    File::open(path.into()).and_then(|file| {
-        let mut build_data = BuildData::new(Some("-".to_string()));
-
-        let stream = BufReader::new(file);
-        let ff = |mut bd: BuildData, line: String| -> FutureResult<BuildData, std::io::Error> {
-            bd.add(&line);
-            println!("LFD: {}", line);
-            future::ok(bd)
-        };
-        lines(stream).fold(build_data, ff)
-    })
+    File::open(path.into())
+        .map_err(|e| Error::from(e))
+        .and_then(|file| {
+            let mut build_data = BuildData::new(Some("-".to_string()));
+            let stream = BufReader::new(file);
+            lines(stream)
+                .map_err(|e| Error::from(e))
+                .fold(build_data, |mut bd, line| -> Result<BuildData> {
+                    bd.add(&line)?;
+                    println!("LFD: {}", line);
+                    Ok(bd)
+                })
+        })
 }
